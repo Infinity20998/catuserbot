24a7452088b0eebc391e00c61a3d50076babbf60
Infinity1335 <77352490+Infinity1335@users.noreply.github.com>
2021-02-09 22:28:52 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -100,7 +100,7 @@ async def on_afk(event):
             else:
                 endtime += f"{m}m {s}s" if m > 0 else f"{s}s"
     current_message_text = event.message.message.lower()
-    if "afk" in current_message_text:
+    if "#afk" in current_message_text:
         # userbot's should not reply to other userbot's
         # https://core.telegram.org/bots/faq#why-doesn-39t-my-bot-see-messages-from-other-bots
         return False
