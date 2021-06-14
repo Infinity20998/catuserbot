366f9edb03d13a03a237c537b7b5d64e70b68597
Infinity1335 <77352490+Infinity1335@users.noreply.github.com>
2021-06-14 15:02:58 +0530

    Update mention.py

--- userbot/plugins/mention.py
+++ userbot/plugins/mention.py
@@ -1,4 +1,4 @@
-from telethon.tl.types import ChannelParticipantsAdmins
+from telethon.tl.types import ChannelParticipantsAdmins as cp
 
 from userbot import catub
 
@@ -8,7 +8,7 @@ plugin_category = "extra"
 
 
 @catub.cat_cmd(
-    pattern="(tagall|all)(?: |$)(.*)",
+    pattern="(all)(?: |$)(.*)",
     command=("tagall", plugin_category),
     info={
         "header": "tags recent 100 persons in the group may not work for all",
@@ -44,12 +44,92 @@ async def _(event):
     chat = await event.get_input_chat()
     reply_to_id = await reply_id(event)
     async for x in event.client.iter_participants(
-        chat, filter=ChannelParticipantsAdmins
+        chat, filter=cp
     ):
         if not x.bot:
             mentions += f"[\u2063](tg://user?id={x.id})"
     await event.client.send_message(event.chat_id, mentions, reply_to=reply_to_id)
     await event.delete()
+    
+    
+@catub.cat_cmd(
+    pattern="tagall ?(.*)",
+    command=("tagall", plugin_category),
+    info={
+        "header": "Tag everyone in a group.",
+        "description": "Mention everyone in the group (with/without custom text)",
+        "options": "If you want text along with tags, use .tagall text",
+        "usage": [
+            "{tr}tagall",
+            "{tr}tagall <text>",
+        ],
+        "examples": [
+            "{tr}tagall",
+            "{tr}tagall Hello!",
+        ],
+    },
+)
+async def _(tag):
+    "To tag everyone in a group."
+
+    if tag.pattern_match.group(1):
+        tagall = tag.pattern_match.group(1)
+    else:
+        tagall = " "
+    prsn1 = await reply_id(tag)
+    chat = await tag.get_input_chat()
+    a_ = 0
+    await tag.delete()
+    async for i in bot.iter_participants(chat):
+        if a_ == 500:
+            break
+        a_ += 5
+        await tag.client.send_message(
+            tag.chat_id,
+            "[{}](tg://user?id={}) {}".format(i.first_name, i.id, tagall),
+            reply_to=prsn1,
+        )
+        sleep(0.1)
+
+
+@catub.cat_cmd(
+    pattern="tagadmin ?(.*)",
+    command=("tagadmin", plugin_category),
+    info={
+        "header": "Tag all admins in a group.",
+        "description": "Mention all the admins in the group (with/without custom text)",
+        "options": "If you want text along with tags, use .tagadmin text",
+        "usage": [
+            "{tr}tagadmin",
+            "{tr}tagadmin <text>",
+        ],
+        "examples": [
+            "{tr}tagadmin",
+            "{tr}tagadmin Spam",
+        ],
+    },
+)
+async def _(tagadmin):
+    "To tag every admin in a group"
+
+    if tagadmin.pattern_match.group(1):
+        tagtxt = tagadmin.pattern_match.group(1)
+    else:
+        tagtxt = " "
+    prsn = await reply_id(tagadmin)
+    chat = await tagadmin.get_input_chat()
+    a_ = 0
+    await tagadmin.delete()
+    async for i in bot.iter_participants(chat, filter=cp):
+        if a_ == 500:
+            break
+        a_ += 5
+        await tagadmin.client.send_message(
+            tagadmin.chat_id,
+            "[{}](tg://user?id={}) {}".format(i.first_name, i.id, tagtxt),
+            reply_to=prsn,
+        )
+        sleep(0.5)
 
 
 @catub.cat_cmd(
