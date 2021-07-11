77d8b50c17bc2be29a235529238f39c896b9a887
Infinity1335 <77352490+Infinity1335@users.noreply.github.com>
2021-07-11 19:24:41 +0530

    Update secret.py

--- userbot/assistant/secret.py
+++ userbot/assistant/secret.py
@@ -18,9 +18,9 @@ async def on_plug_in_callback_query_handler(event):
             ids = [userid, catub.uid]
             if event.query.user_id in ids:
                 encrypted_tcxt = message["text"]
-                reply_pop_up_alert = encrypted_tcxt
+                reply_pop_up_alert = "You are not allowed to see this messagae, better luck next time!"
             else:
-                reply_pop_up_alert = "why were you looking at this shit go away and do your own work, idiot"
+                reply_pop_up_alert = encrypted_tcxt
         except KeyError:
             reply_pop_up_alert = "This message no longer exists in catub server"
     else:
