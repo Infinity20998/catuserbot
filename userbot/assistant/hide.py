4b8deb4d7ff1a019fa79bec9cd9f34305c1b1577
Infinity1335 <77352490+Infinity1335@users.noreply.github.com>
2021-07-30 12:28:02 +0530

    Create hide.py

---
+++ userbot/assistant/hide.py
@@ -0,0 +1,25 @@
+import json
+import os
+import re
+
+from telethon.events import CallbackQuery
+
+from userbot import catub
+
+
+@catub.tgbot.on(CallbackQuery(data=re.compile(b"hide_(.*)")))
+async def on_plug_in_callback_query_handler(event):
+    timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
+    if os.path.exists("./userbot/hide.txt"):
+        jsondata = json.load(open("./userbot/hide.txt"))
+        try:
+            message = jsondata[f"{timestamp}"]
+            ids = [None]
+            if event.query.user_id not in ids:
+                encrypted_tcxt = message["text"]
+                reply_pop_up_alert = encrypted_tcxt
+        except KeyError:
+            reply_pop_up_alert = "This message no longer exists in catub server"
+    else:
+        reply_pop_up_alert = "This message no longer exists "
+    await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
